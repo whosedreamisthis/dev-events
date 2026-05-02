da51ba1e78fbaf0b64a64cb50ecab0a7ee741f75
Dana Sharon <whosedreamisthis@gmail.com>
2026-05-02 15:21:05 -0700

    Add support for image uploads in event creation and implement fetching featured events from API
    - Integrated Cloudinary for image uploads in the `POST /api/events` route.
    - Updated event creation logic to validate image presence and save uploaded images.
    - Refactored Homepage to fetch featured events dynamically from the `/api/events` endpoint.
    - Added route `/api/events/[slug]` for retrieving individual events by slug.
    - Updated `next.config.ts` to allow Cloudinary-hosted images.
    - Installed `cloudinary` dependency.

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { connectToDatabase } from '@/lib/mongodb';
 import { Event } from '@/database/event.model';
-
+import { v2 as cloudinary } from 'cloudinary';
 export async function POST(req: NextRequest) {
   try {
     await connectToDatabase();
@@ -20,34 +20,34 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    // const file = formData.get('image') as File;
-    //
-    // if (!file)
-    //   return NextResponse.json(
-    //     { message: 'Image file is required' },
-    //     { status: 400 }
-    //   );
+    const file = formData.get('image') as File;
+
+    if (!file)
+      return NextResponse.json(
+        { message: 'Image file is required' },
+        { status: 400 }
+      );
 
     let tags = JSON.parse(formData.get('tags') as string);
     let agenda = JSON.parse(formData.get('agenda') as string);
 
-    // const arrayBuffer = await file.arrayBuffer();
-    // const buffer = Buffer.from(arrayBuffer);
-    //
-    // const uploadResult = await new Promise((resolve, reject) => {
-    //   cloudinary.uploader
-    //     .upload_stream(
-    //       { resource_type: 'image', folder: 'DevEvent' },
-    //       (error, results) => {
-    //         if (error) return reject(error);
-    //
-    //         resolve(results);
-    //       }
-    //     )
-    //     .end(buffer);
-    // });
-    //
-    // event.image = (uploadResult as { secure_url: string }).secure_url;
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: 'image', folder: 'DevEvent' },
+          (error, results) => {
+            if (error) return reject(error);
+
+            resolve(results);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
 
     const createdEvent = await Event.create({
       ...event,
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExploreButton from '@/components/ExploreButton';
 import EventCard from '@/components/EventCard';
-import { events } from '@/lib/constants';
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
@@ -17,11 +19,13 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title}>
-              <EventCard {...event}></EventCard>
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event}></EventCard>
+              </li>
+            ))}
         </ul>
       </div>
     </section>
